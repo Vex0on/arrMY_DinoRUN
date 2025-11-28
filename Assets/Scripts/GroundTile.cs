cac93b77250cd42033ede54a4d9eccbb64f829b9
Vex0on <Iplayagame101@gmail.com>
2025-11-28 01:17:04 +0100

    Some visual improvements: - Ground now moves, so it looks like the player is running - Change sprite for obstacle on the floor - Shorten time between game start and first obstacle

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    public float tileWidth = 20f;
+    public SpikeGenerator generator;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * generator.currentSpeed * Time.deltaTime);
+
+        if (transform.position.x < -tileWidth)
+        {
+            transform.position += new Vector3(tileWidth * 2f, 0f, 0f);
+        }
+    }
+}
